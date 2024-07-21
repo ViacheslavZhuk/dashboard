8bf080f4586b6bb4d7794ec40bd755ae24748cdc
ViacheslavZhuk <98652458+ViacheslavZhuk@users.noreply.github.com>
2024-07-21 17:19:45 +0300

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2023 email: 123zhuk123@gmail.com
+    </p>
+  </div>
+);
+
+export default Footer;
